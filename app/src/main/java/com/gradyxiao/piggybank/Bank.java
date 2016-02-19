58356dc3c135548ad71562ce4e59c53c1344adba
grady_xiao <xiao.grady@gmail.com>
2016-02-19 13:48:45 -0500

    Initial Commit

---
+++ app/src/main/java/com/gradyxiao/piggybank/Bank.java
@@ -0,0 +1,29 @@
+package com.gradyxiao.piggybank;
+
+/**
+ * Created by GradyXiao on 2/19/16.
+ */
+public class Bank {
+    private double currentBalance;
+
+    public Bank(){
+        this.currentBalance=0;
+    }
+
+    public void deposit(double amount) throws BankError{
+        this.currentBalance=currentBalance+amount;
+    }
+
+    public void withdraw(double amount) throws BankError{
+        if(currentBalance-amount>=0) {
+            this.currentBalance = currentBalance - amount;
+        } else{
+            throw new BankError("asdf");
+        }
+    }
+
+    public void setCurrentBalance(int amount){
+        this.currentBalance=amount;
+    }
+
+}
---
+++ app/src/main/java/com/gradyxiao/piggybank/BankError.java
@@ -0,0 +1,10 @@
+package com.gradyxiao.piggybank;
+
+/**
+ * Created by GradyXiao on 2/19/16.
+ */
+public class BankError extends Exception {
+    public BankError(String message){
+        super(message);
+    }
+}
